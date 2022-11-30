83a6e01542b01632711296770ee3cc64b07c8fba
PedroArthur <110056969+suassuas20@users.noreply.github.com>
2022-11-30 16:50:15 -0300

    Update relogio.js

--- relogio.js
+++ relogio.js
@@ -1,1 +1,16 @@
-
+function clock(){
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let hora = h + ":" + m + ":" + s;
+    if (h < 10){
+        hora = "0" + h + m + ":" + s;
+    }
+    if(m < 10){
+        hora = h + ":0" + m + ":" + s;
+    }
+    if(s < 10){
+        hora = h + ":" + m + ":0" + s;
+    }
+}
